7a2a1ef230b0a4a193279fcff2873d3a27722e6f
Maika Kailani <queenmab051@gmail.com>
2019-12-16 02:44:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,30 @@
 class Triangle
 
-  attr_accessor :side1, :side2, :side3, :allsides
+  attr_reader :a, :b, :c
 
-  def initialize(side1, side2, side3)
-    @allsides = []
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if @side1 == @side2 && @side2 == @side3
-      return :equilateral
-    elsif @side1 == @side2 && @side1 + @side2 > @side3 || @side1 == @side3 && @side1 + @side3 > @side2 || @side2 == @side3 && @side2 + @side3 > side1
-      return :isosceles
-    elsif @side1 != @side2 && @side2 != @side3 && @side1 + @side2 > @side3
-      return :scalene
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      raise TriangleError if
-        @side1 <=0 || @side2 <= 0 || @side3 <= 0
-      end
+      :scalene
     end
   end
 
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+
   class TriangleError < StandardError
-    def message
-      "not a triangle"
-    end
   end
 end
